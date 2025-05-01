f5db5f67670cf6c9e637fb1e0ce57db8a1e41268
Azamatutemuratov2301 <azamatutemuratov074@gmail.com>
2025-05-01 18:51:58 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,13 @@
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "SIZNING_BOT_TOKEN"
+BOT_TOKEN = "8158588417:AAEHli-xAhqOZewNlXoSfb1XS8-dZ9nwjjg"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Salom! Realized_Profit botga xush kelibsiz.")
+    await update.message.reply_text("Realized_Profit botiga xush kelibsiz!")
 
-def main():
-    app = Application.builder().token(TOKEN).build()
+def run_bot():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
 
-if __name__ == "__main__":
-    main()
