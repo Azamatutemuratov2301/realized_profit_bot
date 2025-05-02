a68e291b90409cb0a667307bd6cdb7ef2ee0242d
Azamatutemuratov2301 <azamatutemuratov074@gmail.com>
2025-05-02 14:14:09 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
-BOT_TOKEN = "8158588417:AAEHli-xAhqOZewNlXoSfb1XS8-dZ9nwjjg"
+from config import BOT_TOKEN, ADMIN_ID
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Realized_Profit botiga xush kelibsiz!")
+    user_id = update.effective_user.id
+    if str(user_id) == str(ADMIN_ID):
+        await update.message.reply_text("Assalomu alaykum, admin! Bot ishga tushdi.")
+    else:
+        await update.message.reply_text("Assalomu alaykum! Sizda to‘liq ruxsat yo‘q.")
 
-def run_bot():
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.run_polling()
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
 
+print("✅ Bot ishga tushdi...")
+app.run_polling()
